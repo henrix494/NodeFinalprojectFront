134bfb85f35ffa0a38f4eb78e5c2587df36c0184
henrix494 <henrix494@gmail.com>
2024-06-15 15:38:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import "./App.css";
+import table from "./assets/table.svg";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Tippy from "@tippyjs/react";
+import TableOp from "./components/TableOp";
+import "tippy.js/dist/tippy.css";
+function App() {
+  const [numOfTables, setNumOfTables] = useState(0);
+  const [tableInfo, setTableInfo] = useState();
+  const [loading, setLoading] = useState(true);
+  const [itemid, setItemId] = useState(null);
+  const [menuopen, setMenuOpen] = useState(false);
+
+  const handleDelete = (id) => {};
+  const handleAddTabe = async () => {
+    const addTable = await axios.post(
+      "https://node-finalproject.vercel.app/tables/addTable"
+    );
+    try {
+      const response = await axios.get(
+        "https://node-finalproject.vercel.app/tables"
+      );
+      setTableInfo(response.data.rows);
+      setNumOfTables(response.data.count);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching tables:", error);
+    }
+  };
+  useEffect(() => {
+    const getAllTables = async () => {
+      try {
+        const response = await axios.get(
+          "https://node-finalproject.vercel.app/tables/"
+        );
+        setTableInfo(response.data.rows);
+        setNumOfTables(response.data.count);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching tables:", error);
+      }
+    };
+
+    getAllTables();
+  }, []);
+  const [tableData, setTableData] = useState();
+
+  const handleTableClick = (id) => {
+    setItemId(id);
+    setMenuOpen(true);
+    getTable(id);
+  };
+  const closeModel = (action) => {
+    setMenuOpen(action);
+  };
+  const getTable = async (id) => {
+    const getData = await axios.get(
+      `https://node-finalproject.vercel.app/tables/getTableById/${id}`
+    );
+    setTableData(getData.data);
+  };
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <div className="mb-10 flex justify-center mt-10">
+            <button
+              onClick={handleAddTabe}
+              className="btn btn-success text-white "
+            >
+              הוסף שולחן
+            </button>
+          </div>
+          <div className="flex flex-wrap gap-10 justify-center ">
+            {tableInfo?.map((item) => {
+              return (
+                <div className=" relative  flex w-[30%] justify-center">
+                  <p className=" absolute">{item.id}</p>
+                  <svg
+                    onClick={() => {
+                      handleTableClick(item.id);
+                    }}
+                    className="w-[30%] hover:opacity-30 cursor-pointer"
+                    width="800px"
+                    fill={item.availability ? " #243c5a" : "red"}
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>table</title>
+                    <path d="M18.76,6l2,4H3.24l2-4H18.76M20,4H4L1,10v2H3v7H5V16H19v3h2V12h2V10L20,4ZM5,14V12H19v2Z" />
+                    <rect width="24" height="24" fill="none" />
+                  </svg>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+      <div className=" ">
+        <TableOp
+          tableData={tableData}
+          id={itemid}
+          close={closeModel}
+          setMenuOpen={setMenuOpen}
+          setTableInfo={setTableInfo}
+          menuopen={menuopen}
+          setTableData={setTableData}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TableOp.jsx
@@ -0,0 +1,155 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+export default function TableOp({
+  id,
+  close,
+  setMenuOpen,
+  setTableInfo,
+  menuopen,
+  tableData,
+  setTableData,
+}) {
+  const [waitersNames, setWaitersNames] = useState();
+  const [selectedWaiterId, setSelectedWaiterId] = useState(null);
+
+  useEffect(() => {
+    const getnames = async () => {
+      const data = await axios.get(
+        "https://node-finalproject.vercel.app/waiters/"
+      );
+      setWaitersNames(data.data.rows);
+    };
+    getnames();
+  }, []);
+  const handleDelete = async () => {
+    const deleteTable = await axios.delete(
+      `https://node-finalproject.vercel.app/tables/deleTableById/${id}`
+    );
+    try {
+      const response = await axios.get(
+        "https://node-finalproject.vercel.app/tables"
+      );
+      setTableInfo(response.data.rows);
+    } catch (error) {
+      console.error("Error fetching tables:", error);
+    }
+    setMenuOpen(false);
+    console.log(deleteTable.data);
+  };
+  const handleSelectChange = (event) => {
+    const selectedId = event.target.value;
+    setSelectedWaiterId(selectedId);
+  };
+  const sendWaiterToDb = async () => {
+    const sendDataToDb = await axios.post(
+      "https://node-finalproject.vercel.app/waiters/AddTableToWaiter",
+      {
+        waiterId: selectedWaiterId,
+        TableId: id,
+      }
+    );
+    try {
+      const response = await axios.get(
+        "https://node-finalproject.vercel.app/tables"
+      );
+      setTableInfo(response.data.rows);
+      const getData = await axios.get(
+        `https://node-finalproject.vercel.app/tables/getTableById/${id}`
+      );
+      setTableData(getData.data);
+      console.log(tableData);
+    } catch (error) {
+      console.error("Error fetching tables:", error);
+    }
+    console.log(sendDataToDb);
+  };
+
+  return (
+    <div
+      className={`fixed right-0 bg-slate-300 h-screen top-0 transition-all duration-500 w-[40dvh]  ${
+        menuopen ? "translate-x-[0%] " : " translate-x-[100%] "
+      }`}
+    >
+      <button
+        onClick={() => close(false)}
+        className={`right-5 top-5 absolute bg-[red] px-5 py-3 hover:opacity-90 rounded-full text-xl text-white`}
+      >
+        X
+      </button>
+      <div className="flex flex-col mt-28 gap-10">
+        <p className="text-center text-xl">
+          אתה בחרתה בשולחן <span className="underline font-bold">{id}</span>
+        </p>
+
+        {tableData?.waiters?.length > 0 ? (
+          <div className="flex flex-col  items-center gap-7">
+            <p>מלצר משויך </p>
+
+            {tableData?.waiters.map((name) => {
+              return (
+                <button className="btn btn-error">
+                  <p>{name.waiterName}</p>
+                </button>
+              );
+            })}
+
+            <div>
+              <h2>הוסף מלצר נוסף</h2>
+              <div>
+                <select onChange={handleSelectChange} className="select w-full">
+                  <option disabled selected>
+                    בחר מלצר
+                  </option>
+                  {waitersNames?.map((name) =>
+                    tableData.waiters.some(
+                      (waiter) => waiter.id === name.id
+                    ) ? null : (
+                      <option key={name.id} value={name.id}>
+                        {name.waiterName}
+                      </option>
+                    )
+                  )}
+                </select>
+
+                <div className="flex justify-center mt-7">
+                  <button
+                    className="btn btn-success"
+                    onClick={() => sendWaiterToDb(selectedWaiterId)}
+                  >
+                    שייך מלצר
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <select onChange={handleSelectChange} className="select w-full ">
+              <option disabled selected>
+                בחר מלצר
+              </option>
+              {waitersNames?.map((name) => (
+                <option key={name.id} value={name.id}>
+                  {name.waiterName}
+                </option>
+              ))}
+            </select>
+            <div className="flex justify-center mt-7">
+              <button
+                className="btn btn-success"
+                onClick={() => sendWaiterToDb(selectedWaiterId)}
+              >
+                שייך מלצר
+              </button>
+            </div>
+          </div>
+        )}
+        <div className="w-full">
+          <button onClick={handleDelete} className="btn btn-warning w-full">
+            מחק
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
